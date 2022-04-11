054a2a25698ebd713129493ace96f81ce5cb9281
umaresso <umarkhatab467@gmail.com>
2022-04-11 07:16:49 +0500

    Added Responsiveness for small screen sizes like 300px

--- src/components/Testimonials/Testimonial.jsx
+++ src/components/Testimonials/Testimonial.jsx
@@ -5,7 +5,7 @@ import "./Testimonial.css";
 function Testimonial({ message, name }) {
   return (
     <div className="testimonial">
-      <mg src="./icons/quote.png" />
+      <img src="./icons/quote.png" />
       <p>{message}</p>
       <h3>{name}</h3>
       <h1>__</h1>
